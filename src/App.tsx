9a84e7fe3cd4fd060b9a6ca980a4b70eea52e895
C Aravindhan <cowara987@gmail.com>
2025-04-14 16:26:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ModelViewer from "./ModelViewer";
+
+function App() {
+  return (
+    <>
+      <ModelViewer modelPath="cat_dispenser.glb" />
+      <ModelViewer modelPath="12_animated_butterflies.glb" />
+      <ModelViewer modelPath="12_animated_butterflies.glb" />
+      <ModelViewer modelPath="12_animated_butterflies.glb" />
+      <ModelViewer modelPath="12_animated_butterflies.glb" />
+    </>
+  );
+}
+
+export default App;
